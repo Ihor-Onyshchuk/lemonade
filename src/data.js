c8be6baa13f6d5a55db703069b70fb947ffad216
ihor <onyshchukihor11@gmail.com>
2020-10-07 09:19:14 +0300

    add mock data

---
+++ src/data.js
@@ -0,0 +1,158 @@
+const data = {
+  500: [
+    {
+      id: 1,
+      question: "the name of Asterix And Obelix's dog?",
+      correct: ["Dogmatix"],
+      answers: [
+        "Lui",
+        "Bosco",
+        "Borys",
+        "Buddy"
+      ]
+    },
+  ],
+  1000: [
+    {
+      id: 2,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  2000: [
+    {
+      id: 3,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  4000: [
+    {
+      id: 4,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  8000: [
+    {
+      id: 5,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  16000: [
+    {
+      id: 6,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  32000: [
+    {
+      id: 7,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  64000: [
+    {
+      id: 8,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  125000: [
+    {
+      id: 9,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  250000: [
+    {
+      id: 10,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  500000: [
+    {
+      id: 11,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+  1000000: [
+    {
+      id: 12,
+      question: "question",
+      correct: ["sfsdfsad"],
+      answers: [
+        "sfsdfsad",
+        "afasdfasd",
+        "sdfasd",
+        "asdfasf"
+      ]
+    },
+  ],
+} 
