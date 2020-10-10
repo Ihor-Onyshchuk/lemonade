acbf57ea9576d9b425746d861cd20609be89153e
ihor <onyshchukihor11@gmail.com>
2020-10-10 13:45:18 +0300

    Updates

---
+++ precache-manifest.5aef9de0edc19951ba64f35ccb008b19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13975307539a9ad59e14a0eb5f6c59bf",
+    "url": "/lemonade/index.html"
+  },
+  {
+    "revision": "7e144cd8fa75f4bccdbc",
+    "url": "/lemonade/static/css/main.3e6684f8.chunk.css"
+  },
+  {
+    "revision": "853c8dd2eae1214f5aa2",
+    "url": "/lemonade/static/js/2.6afbb0f2.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/lemonade/static/js/2.6afbb0f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e144cd8fa75f4bccdbc",
+    "url": "/lemonade/static/js/main.da25c03f.chunk.js"
+  },
+  {
+    "revision": "11006d0d6634ae36c7ce",
+    "url": "/lemonade/static/js/runtime-main.5e61336c.js"
+  },
+  {
+    "revision": "25b31e84ec8dc3d668d12cc2091e8867",
+    "url": "/lemonade/static/media/lucas.25b31e84.svg"
+  }
+]);
