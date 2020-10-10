4c5ee4977f50849f04ea66028693cc3a39ada343
ihor <onyshchukihor11@gmail.com>
2020-10-10 15:53:34 +0300

    change timeout delay

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ import {number_with_delimiter} from 'number_helpers';
 
 export const numberWithDelimiter = (number, config = {}) => number_with_delimiter(number, config);
 
-export const drumroll = (delay = 2000) => new Promise((resolve) => {
+export const drumroll = (delay = 1500) => new Promise((resolve) => {
   setTimeout(() => {
     resolve();
   }, delay);
